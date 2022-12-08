9e7cea30f871327b6b88f1b0527b8c80a51b8c9e
Vlad <gardus.vlad@gmail.com>
2022-12-08 23:07:22 +0200

    Updates

--- ticketing-app/common/src/events/publisher.ts
+++ ticketing-app/common/src/events/publisher.ts
@@ -15,11 +15,13 @@ export abstract class Publisher<T extends Event> {
     const exchangeResponse = await this.channel.assertExchange(this.exchangeName, "topic", { durable: true, autoDelete: false });
     const queueResponse = await this.channel.assertQueue(this.queueName, { autoDelete: true, durable: true });
     await this.channel.bindQueue(queueResponse.queue, exchangeResponse.exchange, this.pattern);
+    console.log(`binded queue ${queueResponse.queue} to exchange ${exchangeResponse.exchange} with pattern ${this.pattern}`);
     return this;
   };
 
   publish = (data: T["data"]) => {
     if (!this.channel) throw new Error("cannot publish a message before building the publisher. Try .build() before .publish()!");
+    console.log(`going to publish message to exchange ${this.exchangeName} with pattern ${this.pattern}`);
     return this.channel.publish(this.exchangeName, this.pattern, Buffer.from(JSON.stringify(data)));
   };
 }
