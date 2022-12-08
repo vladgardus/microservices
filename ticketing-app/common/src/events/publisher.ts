dc4a61de0f29e594a13a80012febd304112f9bc6
Vlad <gardus.vlad@gmail.com>
2022-12-08 18:44:09 +0200

    Updates

--- ticketing-app/common/src/events/publisher.ts
+++ ticketing-app/common/src/events/publisher.ts
@@ -12,14 +12,20 @@ export abstract class Publisher<T extends Event> {
     this.build();
   }
   build = async () => {
+    console.log("this in build ", this)
     this.channel = await this.connection.createChannel();
+    console.log("channel created ", this.channel)
     const exchangeResponse = await this.channel.assertExchange(this.exchangeName, "topic", { durable: true, autoDelete: false });
+    console.log("exchange created")
     const queueResponse = await this.channel.assertQueue(this.queueName, { autoDelete: true, durable: true });
+    console.log("queue created")
     await this.channel.bindQueue(queueResponse.queue, exchangeResponse.exchange, this.pattern);
+    console.log("queue binded")
     return this;
   };
 
   publish = (data: T["data"]) => {
+    console.log("this in publish ", this)
     return this.channel.publish(this.exchangeName, this.pattern, Buffer.from(JSON.stringify(data)));
   };
 }
